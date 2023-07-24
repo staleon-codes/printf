2695b3b8dc6f603027b4851afa95db77a4444c44
Steezcool <132352243+Steezcool@users.noreply.github.com>
2023-07-24 12:26:35 +0200

    Update write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -80,7 +80,6 @@ int write_number(int is_negative, int ind, char buffer[],
 
 /**
  * write_num - Write a number using a bufffer
- * @ind: Index at which the number starts on the buffer
  * @buffer: Buffer
  * @flags: Flags
  * @width: width
