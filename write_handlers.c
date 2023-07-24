3fddc1264bd91441760137e64cda80affe23aba5
Leonard Mbasela <132356293+staleon-codes@users.noreply.github.com>
2023-07-24 13:30:41 +0200

    Update write_handlers.c code

--- write_handlers.c
+++ write_handlers.c
@@ -3,14 +3,14 @@
 /************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
- * @c: char types.
+ * @c: char types
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags.
+ * @flags:  Calculates active flags
  * @width: get width.
  * @precision: precision specifier
  * @size: Size specifier
  *
- * Return: Number of chars printed.
+ * Return: Number of chars printed
  */
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
